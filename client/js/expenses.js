7eb3a9e76a05f92bc2b230086570ed61dd451e29
hightechu2 <hightechu2@uvic.ca>
2019-06-01 13:30:32 -0700

    Create expenses with backend and display

---
+++ client/js/expenses.js
@@ -0,0 +1,19 @@
+
+function createExpense(){
+    fn = function(response, status) {
+        if('error' in response) {
+            alert("{0}: {1}".format(status, response.error.message));
+        }
+        else {
+            alert("Created the expense");
+        }
+    }
+
+    var jsonObj = new Object();
+    jsonObj.transportation = document.getElementById('transportation').value;
+    jsonObj.entertainment = document.getElementById('entertainment').value;
+    jsonObj.nutrition = document.getElementById('nutrition').value;
+
+    APIajax("expenses", "POST", fn, jsonObj);
+    //APIajax("expenses", "GET", fnget);
+}
---
+++ client/js/welcome.js
@@ -0,0 +1,46 @@
+
+popout = document.querySelector('#popout');
+popout.style.display = 'none';
+
+function showMenu(x) {
+    x.classList.toggle("change");
+    if (popout.style.display === "none") {
+        popout.style.display = "";
+    } else {
+        popout.style.display = "none";
+    }
+    //createExpense();
+}
+
+function createExpense(){
+    fn = function(response, status) {
+        if('error' in response) {
+            alert("{0}: {1}".format(status, response.error.message));
+        }
+        else {
+            for (expense in response) {
+                alert(expense.transportation);
+            }
+            alert("Created the expense");
+        }
+    }
+
+    fnget = function(response, status) {
+        if('error' in response) {
+            alert("{0}: {1}".format(status, response.error.message));
+        }
+        else {
+            for (expense in response) {
+                alert(expense.transportation);
+            }
+        }
+    }
+
+    var jsonObj = new Object();
+    jsonObj.transportation = 8;
+    jsonObj.entertainment = 2;
+    jsonObj.nutrition = 180;
+
+    APIajax("expenses", "POST", fn, jsonObj);
+    APIajax("expenses", "GET", fnget);
+}
