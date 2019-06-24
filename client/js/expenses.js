d6199f6c841ca83e4df9d672a10fcc78244b557c
S-Sahota <spencersahota@gmail.com>
2019-06-23 17:55:44 -0700

    Updated expenses

--- client/js/expenses.js
+++ client/js/expenses.js
@@ -1,4 +1,29 @@
 
+function getExpense() {
+    income = document.getElementById('i');
+    transportation = document.getElementById('t');
+    entertainment = document.getElementById('e');
+    nutrition = document.getElementById('n');
+    fnget = function(response, status) {
+        if('error' in response) {
+            alert("{0}: {1}".format(status, response.error.message));
+        } else {
+            for(expense in response) {
+                inc = response[expense].income;
+                tra = response[expense].transportation;
+                ent = response[expense].entertainment;
+                nut = response[expense].nutrition;
+            }
+            //h.innerHTML = nut;
+            income.innerHTML = 'Currently $' + inc;
+            transportation.innerHTML = 'Currently $' + tra;
+            entertainment.innerHTML = 'Currently $' + ent;
+            nutrition.innerHTML = 'Currently $' + nut;
+        }
+    }
+    APIajax("expenses", "GET", fnget);
+}
+
 function updateExpense(){
     fn = function(response, status) {
         if('error' in response) {
@@ -10,11 +35,12 @@ function updateExpense(){
     }
 
     var jsonObj = new Object();
+    jsonObj.income = document.getElementById('income').value;
     jsonObj.transportation = document.getElementById('transportation').value;
     jsonObj.entertainment = document.getElementById('entertainment').value;
     jsonObj.nutrition = document.getElementById('nutrition').value;
     jsonObj.id = 1;
 
-    APIajax("data", "PUT", fn, jsonObj);
+    APIajax("expenses", "PUT", fn, jsonObj);
     //APIajax("expenses", "GET", fnget);
 }
--- client/js/questionnaire.js
+++ client/js/questionnaire.js
@@ -1,5 +1,5 @@
 
-function createExpense(){
+function createExpense() {
     fn = function(response, status) {
         if('error' in response) {
             alert("{0}: {1}".format(status, response.error.message));
@@ -11,11 +11,10 @@ function createExpense(){
 
     var jsonObj = new Object();
     jsonObj.income = document.getElementById('income').value;
-    console.log(document.getElementById('income').value);
-    
     jsonObj.transportation = document.getElementById('transportation').value;
     jsonObj.entertainment = document.getElementById('entertainment').value;
     jsonObj.nutrition = document.getElementById('nutrition').value;
+    jsonObj.id = 1;
 
     APIajax("expenses", "POST", fn, jsonObj);
     //APIajax("expenses", "GET", fnget);
